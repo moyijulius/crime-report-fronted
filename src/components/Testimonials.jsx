abe10755ef5b15b674c94c3eef5447245573458a
Julius Moyi <moyijulius17@gmail.com>
2025-03-30 02:19:03 -0700

    some changes added to URLS

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -18,7 +18,7 @@ function Testimonials() {
   useEffect(() => {
     const fetchTestimonials = async () => {
       try {
-        const response = await axios.get(`${API_URL}/testimonials`);
+        const response = await axios.get(`${API_URL}/api/testimonials`);
         setTestimonials(response.data);
         setLoading(false);
       } catch (err) {
